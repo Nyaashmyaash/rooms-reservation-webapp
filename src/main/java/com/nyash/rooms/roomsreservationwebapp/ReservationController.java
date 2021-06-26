ee09eccd1f0d1fcd7f8f3b89720e14aea356e77d
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-06-26 07:47:05 +0700

    add changes

--- src/main/java/com/nyash/rooms/roomsreservationwebapp/ReservationController.java
+++ src/main/java/com/nyash/rooms/roomsreservationwebapp/ReservationController.java
@@ -1,4 +1,47 @@
 package com.nyash.rooms.roomsreservationwebapp;
 
+import com.nyash.rooms.roomsreservationwebapp.client.RoomReservationService;
+import com.nyash.rooms.roomsreservationwebapp.domain.RoomReservation;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.aop.target.LazyInitTargetSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequestMapping("/reservations")
 public class ReservationController {
+
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+    private final RoomReservationService roomReservationService;
+
+    @Autowired
+    public ReservationController(RoomReservationService roomReservationService) {
+        super();
+        this.roomReservationService = roomReservationService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getReservations(@RequestParam(value = "date", required = false)String dateString, Model model) {
+        String date = dateString;
+        if(StringUtils.isBlank(dateString)){
+            date = this.createTodayDateString();
+        }
+        List<RoomReservation> roomReservations = this.roomReservationService.getRoomReservationsForDate(date);
+        model.addAttribute("roomReservations", roomReservations);
+        return "reservations";
+    }
+
+    private String createTodayDateString() {
+        return DATE_FORMAT.format(new Date());
+    }
 }
--- src/main/java/com/nyash/rooms/roomsreservationwebapp/client/RoomReservationService.java
+++ src/main/java/com/nyash/rooms/roomsreservationwebapp/client/RoomReservationService.java
@@ -1,4 +1,23 @@
 package com.nyash.rooms.roomsreservationwebapp.client;
 
+import com.nyash.rooms.roomsreservationwebapp.domain.Room;
+import com.nyash.rooms.roomsreservationwebapp.domain.RoomReservation;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient("RESERVATIONBUSINESSSERVICES")
+@Component
 public interface RoomReservationService {
+
+    @RequestMapping(value = "/rooms", method = RequestMethod.GET)
+    public List<Room> getAllRooms(@RequestParam(name = "roomNumber", required = false)String roomNumber);
+
+    @RequestMapping(value = "/roomReservations/{date}", method = RequestMethod.GET)
+    public List<RoomReservation> getRoomReservationsForDate(@PathVariable("date") String date);
 }
