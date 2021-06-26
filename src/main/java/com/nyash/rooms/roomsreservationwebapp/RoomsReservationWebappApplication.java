c2dce5cd7b18af1040518b5fc2fea14b6d0b2e1f
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-06-26 07:18:27 +0700

    Initial commit

---
+++ src/main/java/com/nyash/rooms/roomsreservationwebapp/RoomsReservationWebappApplication.java
@@ -0,0 +1,13 @@
+package com.nyash.rooms.roomsreservationwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomsReservationWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomsReservationWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nyash/rooms/roomsreservationwebapp/RoomsReservationWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyash.rooms.roomsreservationwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomsReservationWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
